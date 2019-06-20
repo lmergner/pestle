51d964becdff05c38742cbc756767988f24be862
Luke Mergner <lmergner@gmail.com>
2019-06-20 11:57:44 -0700

    clean up old tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,4 @@
-
-from pytest_pgtap import tests
-
 def test_w_pgtap_fixture(pgtap):
     assert pgtap("""
         SELECT tables_are(ARRAY['post', 'user', 'simple'], 'Should have 3 tables');
     """)
-
-def test_schema_using_assertion_helpers():
-    tests.assert_tables_are('post', 'user', 'simple', msg='blarg')
-    tests.assert_columns_are('post', 'oid', 'created', 'modified', 'extras')
