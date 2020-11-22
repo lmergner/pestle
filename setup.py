5eeb47d4de37b15670e591cc73da886afcb16443
Luke Mergner <lmergner@gmail.com>
2020-11-22 10:39:56 -0800

    Requirement changes
    add extras for async requirements
    bump versions for test_requires
    drop falcon and marshmallow as hard requirements

--- setup.py
+++ setup.py
@@ -20,18 +20,21 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="An opinionated collection of ORM mixins and tools for SQLAlchemy projects",
     install_requires=[
         "sqlalchemy>=1.3.17",
         "passlib>=1.7.2",
         "bcrypt>=3.1.7",
+        "gunicorn==20.0.4",
         # TODO:  psycopg should be built form source according to the docs
         "psycopg2-binary>=2.8.5",
-        "falcon==2.0.0",
-        "marshmallow==3.8.0",
-        "gunicorn==20.0.4",
     ],
+    extras_require={
+        "async": ["SQLAlchemy==1.4.0b1", "asyncpg==0.21.0", "uvloop==0.14.0"],
+        "sync": [],
+    },
     license="MIT license",
     long_description=long_description,
     keywords="sqlalchemy",
@@ -39,8 +42,8 @@ setup(
     packages=["pestle"],
     test_suite="tests",
     tests_require=[
-        "pytest==5.4.2",
-        "pytest-cov==2.9.0",
+        "pytest>=6.1.1",
+        "pytest-cov>=2.9.0",
         "pytest-pgtap @ git+https://github.com/lmergner/pytest-pgtap@v0.1.0#egg=pytest_pgtap",
         # https://python-packaging.readthedocs.io/en/latest/dependencies.html#packages-not-on-pypi
     ],
